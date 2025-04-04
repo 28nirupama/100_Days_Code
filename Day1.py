69830426e27a6b027c7208eec70507b39842f214
H N NIRUPAMA <158119116+28nirupama@users.noreply.github.com>
2025-04-04 19:48:42 +0530

    Day1_bandnamegenerator.py

--- Day1.py
+++ Day1.py
@@ -1,7 +1,15 @@
-#band name
+# A simple script to generate a fun band name using user input.
+
+# Welcome message
 print("Welcome to the Band Name Generator")
-var=input("What's the name of the city you grew up in?")
+
+# Get city name
+var = input("What's the name of the city you grew up in?")
 print(var)
-var2=input("What's your pet's name?")
+
+# Get pet name
+var2 = input("What's your pet's name?")
 print(var2)
-print("Your band name could be "+var+" "+ var2)
+
+# Generate band name
+print("Your band name could be " + var + " " + var2)
